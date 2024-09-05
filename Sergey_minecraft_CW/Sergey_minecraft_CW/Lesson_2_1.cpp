daa7fbe6a8d8478027a3e353ace367fd04fa3d59
Sergey0909087 <128168133+Sergey0909087@users.noreply.github.com>
2024-09-05 12:12:06 +0300

    Add files via upload

---
+++ Sergey_minecraft_CW/Sergey_minecraft_CW/Lesson_2_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	// system("chcp 1251");
+	setlocale(LC_ALL, "Rus");
+	std::cout << "hello world!\n";
+	std::cout << "Привет\n";
+	system("pause");
+	return 0;
+}
---
+++ Sergey_minecraft_CW/Sergey_minecraft_CW/Lesson_2_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	std::cout << "Иванов Сергей Витальевич\nГод рождения: 2007\nПол: Мужской";
+	return 0;
+}
